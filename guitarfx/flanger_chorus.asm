607e9f49effcd6b79a5123210939ab845a3fdda8
Casper <ckn16@student.aau.dk>
2019-05-13 16:06:20 +0200

    IIR - Flanger and Chorus implemented

--- guitarfx/flanger_chorus.asm
+++ guitarfx/flanger_chorus.asm
@@ -1,71 +1,153 @@
-b0					.set	0x03e8				; signed Q16,0
-a0					.set	0x07d0				; unsigned Q15,1
-resetZero			.set	0x0000
-direct_path			.set	0x4000
-feedback_path		.set	0x6000
-ADCR				.set	0x002A2D
+ .copy "oscillator_32bit.asm"
+;b0     .set 0x03e8    ; signed Q16,0
+;a0     .set 0x07d0    ; unsigned Q15,1
+;resetZero   	.set 0x0000
+direct_path   	.set 0x4000
+feedback_path  	.set 0x0CCC
+ADCR    		.set 0x002A2D
+voices    		.set    0x0002
 
-G:					.usect ".vars", 1       ;Forward Gain: Q15
-F:					.usect ".vars", 1		;Feedback Gain: Q15
-bd:					.usect ".vars", 1		;Base delay is signed: Q16,0
-amp:				.usect ".vars", 1		;Amplitude is unsigned: Q15,1
-delay_output:		.usect ".vars", 2,2,2
-interpol:			.usect ".vars", 1		;Needed so we are able to do a unsigned multiplication
-noise_shaping:		.usect ".vars", 1
+		.sect  ".ivars"      ;Feedback Gain: Q15
+ 		.align 2
+K1:		.word 0x0001      ;HI(K1) for voice 0
+ 		.word 0x1284      ;LO(K1) for voice 0      0x00001B73 = (0,05) 0,1 Hz
+		.word 0x0001      ;-- Voice 1
+		.word 0x1284      ;-- Voice 1     0x000036E7 = (0,1) 0,2 Hz
+		.word 0x0001      ;-- Voice 2
+		.word 0x1284      ;-- Voice 2     0x00006DCE = (0,2) 0,4 Hz
+		.word 0x0000      ;-- Voice 3
+		.word 0xA4B6      ;-- Voice 3      0x00011284 = (0,5) 1 Hz
+		.word 0x0000      ;-- Voice 4
+		.word 0xA4B6      ;-- Voice 4
 
+		.align 2
+K2: 	.word 0x0002      ;HI(K2) for voice 0
+		.word 0x2508      ;LO(K2) for voice 0   0x000036E7 = (0,05) 0,1 Hz
+		.word 0x0002      ;-- Voice 1
+		.word 0x2508      ;-- Voice 1     0x00006DCE = (0,1) 0,2 Hz
+		.word 0x0002      ;-- Voice 2
+		.word 0x2508      ;-- Voice 2     0x0000DB9D = (0,2) 0,4 Hz
+		.word 0x0001      ;-- Voice 3
+		.word 0x496B      ;-- Voice 3     0x00022508 = (0,5) 1 Hz
+		.word 0x0001      ;-- Voice 4
+		.word 0x496B      ;-- Voice 4
 
-	.ref oscillator_inc32, un, vn, xn
-	.def _flanger_chorus_reset, _flanger_effect
-	.text
+ 		.align 2
+un: 	.word 	0x7FFF      ;HI(un) Start value for sin(n*w)? = 1
+		.word 	0xFFFF      ;LO(un) Start value for sin(n*w) = 1
+		.word	0x7FFF      ;HI(un) Start value for sin(n*w)? = 1
+		.word 	0xFFFF      ;LO(un) Start value for sin(n*w) = 1
+		.word 	0x7FFF      ;HI(un) Start value for sin(n*w)? = 1
+		.word 	0xFFFF      ;LO(un) Start value for sin(n*w) = 1
+		.word 	0x7FFF      ;HI(un) Start value for sin(n*w)? = 1
+		.word 	0xFFFF      ;LO(un) Start value for sin(n*w) = 1
+		.word 	0x7FFF      ;HI(un) Start value for sin(n*w)? = 1
+		.word 	0xFFFF      ;LO(un) Start value for sin(n*w) = 1
+
+		.align 2
+vn: 	.word 	0x0000      ;HI(vn) Start value for cos(n*w)? = 0
+		.word 	0x0000      ;LO(vn) Start value for cos(n*w) = 0
+		.word 	0x0000      ;HI(vn) Start value for cos(n*w)? = 0
+		.word 	0x0000      ;LO(vn) Start value for cos(n*w) = 0
+		.word 	0x0000      ;HI(vn) Start value for cos(n*w)? = 0
+		.word 	0x0000      ;LO(vn) Start value for cos(n*w) = 0
+		.word 	0x0000      ;HI(vn) Start value for cos(n*w)? = 0
+		.word 	0x0000      ;LO(vn) Start value for cos(n*w) = 0
+		.word 	0x0000      ;HI(vn) Start value for cos(n*w)? = 0
+		.word 	0x0000      ;LO(vn) Start value for cos(n*w) = 0
+
+ 		.align 2
+F: 		.word 	0x3333
+		.word 	0x3333
+		.word 	0x3333
+		.word 	0x000A
+		.word 	0x0CCC
+
+ 		.align 2
+bd:	 	.word	0x00FF 		; Base delay is signed: Q16,0
+		.word 	0x0000 		; It is necessary to have long word alignment for delay calculate to work, therefore zero padding is used.
+		.word 	0x00E7
+		.word 	0x0000
+		.word 	0x0AB9
+		.word 	0x0000
+		.word 	0x00FF
+		.word 	0x0000
+		.word 	0x7FFF
+
+		.align 2
+amp:	.word 	0x00FF		; Amplitude is unsigned: Q15,1
+		.word 	0x0000 		; It is necessary to have long word alignment for delay calculate to work, therefore zero padding is used.
+		.word 	0x00E7
+		.word 	0x0000
+		.word 	0x0AB9
+		.word 	0x0000
+		.word 	0x00FF
+		.word 	0x0000
+
+G:     					.usect ".vars", 1       	;Forward Gain: Q15
+delay_output:  			.usect ".vars", 2,2,2
+interpol:   			.usect ".vars", 1  			;Needed so we are able to do a unsigned multiplication
+noise_shaping:  		.usect ".vars", 1
+aditional_voices: 		.usect ".vars", 1
+		;.ref oscillator_inc32,
+		;.ref un, vn
+		.ref xn
+		.def _flanger_chorus_reset, _flanger_effect, _chorus_effect, _oscillator_reset32
+		.text
 
 _flanger_chorus_reset:
-	MOV #a0, *(#amp)
-	MOV #b0, *(#bd)
+	;MOV #a0, *(#amp)
+	;MOV #b0, *(#bd)
 	MOV #direct_path, *(#G)
-	MOV #feedback_path, *(#F)
+	;MOV #feedback_path, *(#F)
 	MOV #resetZero, *(#delay_output)
 	MOV #resetZero, *(#delay_output+1)
 	MOV #resetZero, *(#noise_shaping)
+	MOV #voices, *(#aditional_voices)
 
 * Caluclate M(n) = (amplitude * sin(omeg*n)) + base_delay
-delay_calculate:
+delay_calculate: .macro
 *Setup to calculate M(n)
-	AMOV #un, 			 	XAR0				;Un is Q31
-	AMOV #delay_output,		XAR1				;Out is Q15,32
-	AMOV #amp,				XAR2
+	MOV BRC0, T0
+	SFTS T0, #1         							;Un is long word aligned, thus amp had to be long word aligned to make the addressing offset long word aligned
+	AMOV #un,      XAR0    							;Un is Q31
+	AMOV #delay_output,  XAR1    					;Out is Q15,32
+	AMOV #amp,    XAR2
+	AMOV #bd,    XAR3
 *Calculate product
-	AMAR *AR0+									; AR1 = LO(out)
-	MPYM uns(*AR0-), uns(*AR2), AC1				; AC1 = Amp * LO(Un)
-	MACM *AR0, uns(*AR2), AC1 >> #16, AC1 		; AC1 = Amp * LO(Un) + Amp * HI(Un) *Product: Q16,16
+	AMAR *AR0+         								;AR1 = LO(out)
+	MPYM uns(*AR0(T0)), uns(*AR2(T0)), AC1    		;AC1 = Amp * LO(Un)
+	|| SUB #1, AR0
+	MACM *AR0(T0), uns(*AR2(T0)), AC1 >> #16, AC1   ;AC1 = Amp * LO(Un) + Amp * HI(Un) *Product: Q16,16
 *Calculate sum (basedelay)
-	ADD *(#bd) << #16, AC1						; Upper bits is now Base delay, lower is used for interpolation
-	MOV AC1, dbl(*AR1)							; Save to memeory
-*Post increment Oscillator
-	CALL oscillator_inc32
-	RET
+	ADD *AR3(T0) << #16, AC1      					;Upper bits is now Base delay, lower is used for interpolation
+	MOV AC1, dbl(*AR1)       						;Save to memeory
+	.endm
 
 * y[n] = D * (x[n] + G * y[n-M(n)])
 _flanger_effect:
+*Pre increment Oscillator
+	;CALL oscillator_inc32
+	MOV #0, BRC0          							;Needed to make the oscillator run on voice 0, probably redundant loops leave BRC0 = 0
+	oscillator_inc32
 *The flanger effect relys on a sin delay
-	CALL delay_calculate
-
+	delay_calculate
 *Setup to find a
 	; y[n] = *AR6
-	MOV *(#delay_output), T0							;FLOOR(M(n))
-*Calculate a = y[n-FLOOR(M(n))] - y[n-FLOOR(M(n))-1]
-	MOV *AR6(T0), AC0  						    		;AC0 = y[n-FLOOR(M(n))]
-	ADD #1, T0											;Modify AR6 offset
-	SUB *AR6(T0), AC0									;AC0 = y[n-FLOOR(M(n))] - y[n-FLOOR(M(n))-1]
+	MOV *(#delay_output), T0      					;FLOOR(M(n))
+*Calculate a = y[n-FLOOR(M(n))] - y[n-FLOOR(M(n))-1] ; HAR ÆNDRET INTERPOLATION, TROR VI REGNEDE Y1-Y2 FØR, NU SKULLE DET GERNE VÆRE Y2-Y1, MEN TJEK EFTER!
+	SUB #1, T0
+	MOV *AR6(T0), AC0 					            ;AC0 = y[n-FLOOR(M(n))]
+	ADD #1, T0           							;Modify AR6 offset
+	SUB *AR6(T0), AC0         						;AC0 = y[n-FLOOR(M(n))] - y[n-FLOOR(M(n))-1]
 	MOV AC0, *(#interpol)
-
 *Setup to interpolate
-	AMOV #delay_output+1, 	XAR0
-	AMOV #interpol, 		XAR1
+	AMOV #delay_output+1,  XAR0
+	AMOV #interpol,   XAR1
 *Interpolate a*LO(delayOutput)+y[n-FLOOR(M(n))]
-	MPYM uns(*AR0), *AR1, AC0							;AC0 = a*LO(delayOutput), LO(delayOutput) = fractional part
-	SUB #1, T0											;Reset T0 to point at y[n-FLOOR(M(n))]
-	ADD *AR6(T0) << #16, AC0							;AC0 = a*LO(delayOutput)+y[n-FLOOR(M(n))]
-
+	MPYM uns(*AR0), *AR1, AC0       				;AC0 = a*LO(delayOutput), LO(delayOutput) = fractional part
+	SUB #1, T0           							;Reset T0 to point at y[n-FLOOR(M(n))]
+	ADD *AR6(T0) << #16, AC0       					;AC0 = a*LO(delayOutput)+y[n-FLOOR(M(n))]
 *Setup for flanger effect
 	; AC0 = y[n-M(n)]
 	AMOV #xn, XAR1
@@ -73,9 +155,46 @@ _flanger_effect:
 	BSET FRCT
 	MPYM *(#F), AC0
 	ADD *AR1 << #16, AC0
- 	MPYM *(#G), AC0
-	;ADD *(#noise_shaping), AC0							; ADD noise shaping
+	MPYM *(#G), AC0
+	ADD *(#noise_shaping), AC0       				;ADD noise shaping
 	MOV HI(AC0), *AR1
-	;MOV AC0, *(#noise_shaping)							; Save noise for next repetition
+	MOV AC0, *(#noise_shaping)       				;Save noise for next repetition
 	BCLR FRCT
+	RET
+
+
+_chorus_effect:
+*Post increment Oscillator
+	MOV #0, AC3           							;Zero AC3 so initial sum is 0 + voice(BRC0)
+	AMOV #F, XAR4          							;AR4 points to first feedback gain
+	MOV *(#aditional_voices), BRC0      			;Loop *(#aditional_voices)+1
+	RPTB END          								;Loop everything until lable END, TOO MANY BYTES FOR LOCAL
+	oscillator_inc32        						;Calculate sine value for voice(BRC0)
+*The chorus effect relys on a sin delay
+	delay_calculate
+*Setup to find a
+	; y[n] = *AR6
+	MOV *(#delay_output), T0       					;FLOOR(M(n))
+*Calculate a = y[n-FLOOR(M(n))] - y[n-FLOOR(M(n))-1]
+	MOV *AR6(T0), AC0     				         	;AC0 = y[n-FLOOR(M(n))]
+	ADD #1, T0           							;Modify AR6 offset
+	SUB *AR6(T0), AC0      							;AC0 = y[n-FLOOR(M(n))] - y[n-FLOOR(M(n))-1]
+	MOV AC0, *(#interpol)
+*Setup to interpolate
+	AMOV #delay_output+1,  XAR0
+	AMOV #interpol,   XAR1
+*Interpolate a*LO(delayOutput)+y[n-FLOOR(M(n))]
+	MPYM uns(*AR0), *AR1, AC0       				;AC0 = a*LO(delayOutput), LO(delayOutput) = fractional part
+	SUB #1, T0           							;Reset T0 to point at y[n-FLOOR(M(n))]
+	ADD *AR6(T0) << #16, AC0       					;AC0 = a*LO(delayOutput)+y[n-FLOOR(M(n))]
+	MOV BRC0, T0
+	MPYM *AR4(T0), AC0         						;Multiply with feedback gain for each voice
+END ADD AC0 << #1, AC3         					;Sum output of all voices
+	AMOV #xn, XAR1
+	ADD *AR1 << #16, AC3
+	MPYM *(#G), AC3
+	SFTS AC3, #1
+	ADD *(#noise_shaping), AC3       				;ADD noise shaping
+	MOV HI(AC3), *AR1
+	MOV AC3, *(#noise_shaping)       				;Save noise for next repetition
 	RET
--- guitarfx/oscillator_32bit.asm
+++ guitarfx/oscillator_32bit.asm
@@ -1,81 +1,122 @@
-u0H					.set	0x7FFF		; Initial Value
-u0L					.set 	0xFFFF		; Initial Value
-v0H					.set 	0x0000 		; Initial Value
-v0L					.set 	0x0000		; Initial Value
-k1H					.set    0x0000		; Initial Value
-k1L					.set 	0xa4b6		; Initial Value
-k2H					.set    0x0001		; Initial Value
-k2L					.set 	0x496b		; Initial Value
-resetZero			.set	0x0000
+u0H     	.set 0x7FFF  ; Initial Value
+u0L     	.set  0xFFFF  ; Initial Value
+v0H     	.set  0x0000   ; Initial Value
+v0L     	.set  0x0000  ; Initial Value
+K1H     	.set    0x0000  ; Initial Value
+K1L     	.set  0x1B73  ; Initial Value
+K2H     	.set    0x0000  ; Initial Value
+K2L    		.set  0x36E7  ; Initial Value
+resetZero   .set 0x0000
+; .sect ".ivars"
+; .align 2
+;K1: .word 0x0000      ;HI(K1) for voice 0
+; .word 0xA4B6      ;LO(K1) for voice 0      0x00001B73 = (0,05) 0,1 Hz
+; .word 0x0000      ;-- Voice 1
+; .word 0x36E7      ;-- Voice 1     0x000036E7 = (0,1) 0,2 Hz
+; .word 0x0000      ;-- Voice 2
+; .word 0x6DCE      ;-- Voice 2     0x00006DCE = (0,2) 0,4 Hz
+; .word 0x0000      ;-- Voice 3
+; .word 0xA4B6      ;-- Voice 3
+; .word 0x0000      ;-- Voice 4
+; .word 0xA4B6      ;-- Voice 4
 
-k1:		.usect ".vars", 2,2,2
-k2:		.usect ".vars", 2,2,2
-un:		.usect ".vars", 2,2,2
-vn:		.usect ".vars", 2,2,2
-out:	.usect ".vars", 2,2,2
+;K2: .word 0x0000      ;HI(K2) for voice 0
+; .word 0xDB9D      ;LO(K2) for voice 0   0x000036E7 = (0,05) 0,1 Hz
+; .word 0x0000      ;-- Voice 1
+; .word 0x6DCE      ;-- Voice 1     0x00006DCE = (0,1) 0,2 Hz
+; .word 0x0000      ;-- Voice 2
+; .word 0xDB9D      ;-- Voice 2     0x0000DB9D = (0,2) 0,4 Hz
+; .word 0x0001      ;-- Voice 3
+; .word 0x496B      ;-- Voice 3
+; .word 0x0001      ;-- Voice 4
+; .word 0x496B      ;-- Voice 4
+
+;un: .word 0x7FFF      ;HI(un) Start value for sin(n*w)? = 1
+; .word 0x7FFF      ;LO(un) Start value for sin(n*w) = 1
+; .word 0x7FFF      ;HI(un) Start value for sin(n*w)? = 1
+; .word 0x7FFF      ;LO(un) Start value for sin(n*w) = 1
+; .word 0x7FFF      ;HI(un) Start value for sin(n*w)? = 1
+; .word 0x7FFF      ;LO(un) Start value for sin(n*w) = 1
+; .word 0x7FFF      ;HI(un) Start value for sin(n*w)? = 1
+; .word 0x7FFF      ;LO(un) Start value for sin(n*w) = 1
+; .word 0x7FFF      ;HI(un) Start value for sin(n*w)? = 1
+; .word 0x7FFF      ;LO(un) Start value for sin(n*w) = 1
+
+;vn: .word 0x0000      ;HI(vn) Start value for cos(n*w)? = 0
+; .word 0x0000      ;LO(vn) Start value for cos(n*w) = 0
+; .word 0x0000      ;HI(vn) Start value for cos(n*w)? = 0
+; .word 0x0000      ;LO(vn) Start value for cos(n*w) = 0
+; .word 0x0000      ;HI(vn) Start value for cos(n*w)? = 0
+; .word 0x0000      ;LO(vn) Start value for cos(n*w) = 0
+; .word 0x0000      ;HI(vn) Start value for cos(n*w)? = 0
+; .word 0x0000      ;LO(vn) Start value for cos(n*w) = 0
+; .word 0x0000      ;HI(vn) Start value for cos(n*w)? = 0
+; .word 0x0000      ;LO(vn) Start value for cos(n*w) = 0
 
 
-	.def  oscillator_inc32,  _oscillator_reset32, un, vn
-	.text
-
+ ;.def  oscillator_inc32,
+ ;.def un, vn,
+ ;.def  _oscillator_reset32
+ .text
 _oscillator_reset32:
-	MOV #u0H, *(#un)							; Setting HI(Un) = 0.999999
-	MOV #u0L, *(#un+1)							; Setting LO(Un) = 999999
-	MOV #v0H, *(#vn)							; Setting HI(vn) = 0
-	MOV #v0L, *(#vn+1)							; Setting LO(Vn) = 0
-	MOV #k1H, *(#k1)							; *Redundant
-	MOV #k1L, *(#k1+1)							; *Redundant
-	MOV #k2H, *(#k2)							; *Redundant
-	MOV #k2L, *(#k2+1)							; *Redundant
-	MOV #resetZero, *(#out)						; *Redundant
-	MOV #resetZero, *(#out+1)					; *Redundant
-	RET
+; MOV #u0H, *(#un)       ; Setting HI(Un) = 0.999999
+; MOV #u0L, *(#un+1)       ; Setting LO(Un) = 999999
+; MOV #v0H, *(#vn)       ; Setting HI(vn) = 0
+; MOV #v0L, *(#vn+1)       ; Setting LO(Vn) = 0
+; MOV #K1H, *(#K1)       ; *Redundant
+; MOV #K1L, *(#K1+1)       ; *Redundant
+; MOV #K2H, *(#K2)       ; *Redundant
+; MOV #K2L, *(#K2+1)       ; *Redundant
+ RET
 
-
-
-oscillator_inc32:
-	BSET FRCT
-	BSET SATD
+oscillator_inc32: .macro
+	 BSET FRCT
+	 BSET SATD
 *Setup to calculate wn
-	AMOV #vn, 		XAR0					; *Vn is Q31
-	AMOV #k1,		XAR1					; *K1 is Q31
-	AMOV #un,		XAR2					; *Un is Q31
-
+	 AMOV #vn,   XAR0     ; *Vn is Q31
+	 AMOV #K1,  XAR1     ; *K1 is Q31
+	 AMOV #un,  XAR2     ; *Un is Q31
+	 MOV BRC0, T0       ; Load current voice iteration into T0 to use for offset
+	 SFTS T0 , #1       ; Multiply by two because all constants are long word aligned
 *Calculate Wn = Un - K1 *Vn
-	AMAR *AR1+								; Point to LO(K1)
-	MPYM *AR0+, uns(*AR1-), AC1				; AC1 = HI(Vn)*LO(K1) 										(SAVE)
-	MACM uns(*AR0-), *AR1, AC1 				; AC1 = HI(Vn)*LO(K1) + LO(Vn)*Hi(K1) 						(ACCUMULATE)
-	MACM *AR0, *AR1, AC1 >> #16, AC1		; AC1 = HI(Vn)*LO(K1) + LO(Vn)*Hi(K1) + HI(Vn)*HI(K1)		(ACCUMULATE)
-	SUB AC1, dbl(*AR2), AC1					; AC1 = Un - AC1											(SUBSTRACT)
-	MOV AC1, dbl(*AR2)						; AR2 = Wn (We no longer need Un)
-
+	 AMAR *AR1+        ; Point to LO(K1)
+	 MPYM *AR0(T0), uns(*AR1(T0)), AC1    ; AC1 = HI(Vn)*LO(K1)           (SAVE)
+	  || ADD #1, AR0
+	 SUB #1, AR1
+	 MACM uns(*AR0(T0)), *AR1(T0), AC1     ; AC1 = HI(Vn)*LO(K1) + LO(Vn)*Hi(K1)       (ACCUMULATE)
+	  || SUB #1, AR0
+	 MACM *AR0(T0), *AR1(T0), AC1 >> #16, AC1  ; AC1 = HI(Vn)*LO(K1) + LO(Vn)*Hi(K1) + HI(Vn)*HI(K1)  (ACCUMULATE)
+	 SUB AC1, dbl(*AR2(T0)), AC1     ; AC1 = Un - AC1           (SUBSTRACT)
+	 MOV AC1, dbl(*AR2(T0))      ; AR2 = Wn (We no longer need Un)
 *Setup to calculate Vn+1
-	; AR0 = HI(Vn)
-	AMOV #k2,		XAR1					; *K2 is Q31
-	; AR2 = HI(Wn)
-
+	 ; AR0 = HI(Vn)
+	 AMOV #K2,  XAR1     ; *K2 is Q31
+	 ; AR2 = HI(Wn)
 *Calculate Vn+1 = Vn + K2 * Wn
-	AMAR *AR1+								; Point to LO(K2)
-	MPYM *AR2+, uns(*AR1-), AC1				; AC1 = HI(Wn)*LO(K2) 										(SAVE)
-	MACM uns(*AR2-), *AR1, AC1 				; AC1 = HI(Wn)*LO(K2) + LO(Wn)*Hi(K2) 						(ACCUMULATE)
-	MACM *AR2, *AR1, AC1 >> #16, AC1		; AC1 = HI(Wn)*LO(K2) + LO(Wn)*Hi(K2) + HI(Wn)*HI(K2)		(ACCUMULATE)
-	ADD dbl(*AR0), AC1						; AC1 = Vn + AC1											(SUBSTRACT)
-	MOV AC1, dbl(*AR0)						; AR0 = Vn+1
-
+	 AMAR *AR1+        ; Point to LO(K2)
+	 MPYM *AR2(T0), uns(*AR1(T0)), AC1    ; AC1 = HI(Wn)*LO(K2)           (SAVE)
+	  || ADD #1, AR2
+	 SUB #1, AR1
+	 MACM uns(*AR2(T0)), *AR1(T0), AC1     ; AC1 = HI(Wn)*LO(K2) + LO(Wn)*Hi(K2)       (ACCUMULATE)
+	  || SUB #1, AR2
+	 MACM *AR2(T0), *AR1(T0), AC1 >> #16, AC1  ; AC1 = HI(Wn)*LO(K2) + LO(Wn)*Hi(K2) + HI(Wn)*HI(K2)  (ACCUMULATE)
+	 ADD dbl(*AR0(T0)), AC1      ; AC1 = Vn + AC1           (SUBSTRACT)
+	 MOV AC1, dbl(*AR0(T0))      ; AR0 = Vn+1
 *Setup to calculate Un+1
-	; AR0 = HI(Vn)
-	AMOV #k1,		XAR1					; *K1 is Q31
-	; AR2 = HI(Wn)
-
+	 ; AR0 = HI(Vn)
+	 AMOV #K1,  XAR1     ; *K1 is Q31
+	 ; AR2 = HI(Wn)
 *Calculate Un+1 = Wn - K1 * Vn+1
-	AMAR *AR1+								; Point to LO(K1)
-	MPYM *AR0+, uns(*AR1-), AC1				; AC1 = HI(Vn+1)*LO(K2) 									(SAVE)
-	MACM uns(*AR0-), *AR1, AC1 				; AC1 = HI(Vn+1)*LO(K2) + LO(Vn+1)*Hi(K2) 					(ACCUMULATE)
-	MACM *AR0, *AR1, AC1 >> #16, AC1		; AC1 = HI(Vn+1)*LO(K2) + LO(Vn+1)*Hi(K2) + HI(Vn+1)*HI(K2)	(ACCUMULATE)
-	SUB AC1, dbl(*AR2), AC1					; AC1 = Wn - AC1											(SUBSTRACT)
-	MOV AC1, dbl(*AR2)						; AR2 = Un
-
-	;MOV HI(AC1), T0
-	BCLR FRCT
-	BCLR SATD
-	RET
+	 AMAR *AR1+        ; Point to LO(K1)
+	 MPYM *AR0(T0), uns(*AR1(T0)), AC1    ; AC1 = HI(Vn+1)*LO(K2)          (SAVE)
+	  || ADD #1, AR0
+	 SUB #1, AR1
+	 MACM uns(*AR0(T0)), *AR1(T0), AC1     ; AC1 = HI(Vn+1)*LO(K2) + LO(Vn+1)*Hi(K2)      (ACCUMULATE)
+	  || SUB #1, AR0
+	 MACM *AR0(T0), *AR1(T0), AC1 >> #16, AC1  ; AC1 = HI(Vn+1)*LO(K2) + LO(Vn+1)*Hi(K2) + HI(Vn+1)*HI(K2) (ACCUMULATE)
+	 SUB AC1, dbl(*AR2(T0)), AC1     ; AC1 = Wn - AC1           (SUBSTRACT)
+	 MOV AC1, dbl(*AR2(T0))      ; AR2 = Un
+	 ;MOV HI(AC1), T0
+	 BCLR FRCT
+	 BCLR SATD
+ .endm
